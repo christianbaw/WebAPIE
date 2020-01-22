a40deaa3a9d9ed4fce0de3f4072e1522f8a0c321
Christian G <Chris@LENOVO>
2020-01-22 11:37:22 -0700

    Added new models

---
+++ WebAPI.Core/Models/PersonModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WebAPI.Core.Models
+{
+
+    [Table("Person", Schema = "Person")]
+    public class PersonModel
+    {
+        
+        [Key]
+        public int BusinessEntityID { get; set; }
+        public string PersonType { get; set; }
+        public Boolean? NameStyle { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Suffix { get; set; }
+        public int EmailPromotion { get; set; }
+
+        public string PersonPicture { get; set; }
+
+    }
+}
